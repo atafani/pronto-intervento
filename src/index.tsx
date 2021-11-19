25f90561359eab891e68e960181db8cf5de9e19d
rei_musabelli <rei.musabelli@localweb.it>
2021-11-19 13:07:52 +0100

    refactor: readded chakra provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import "./pronto/assets/globals/global.scss";
 import ProntoApp from "./pronto-app/ProntoApp";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
   <React.StrictMode>
+    <ChakraProvider>
       <ProntoApp />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
